c59601e0572b959c11fb679744fe2dffaf22f7b7
z1kary <vicburet@orange.fr>
2020-11-27 23:50:08 +0000

    Showing articles details

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-    
+    validates :title, presence: true, length: { minimum: 5, maximum: 60 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 255 }
 end
