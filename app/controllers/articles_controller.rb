2e71f326cb94aa28f217a666c6e52b60677005e9
z1kary <vicburet@orange.fr>
2020-11-28 02:27:54 +0000

    Add returns of errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,15 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     def new
+        @article = Article.new
     end
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        redirect_to article_path(@article)
+        if @article.save
+            flash[:notice] = "Article was created successfully"
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 end
