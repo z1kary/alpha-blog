d72b1fee19aba74ccefbc48a43113c5604702f59
z1kary <vicburet@orange.fr>
2020-11-28 05:27:32 +0000

    Interface to modify article table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -16,5 +19,19 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 end
