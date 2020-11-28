302432849d7e7affb7c435ab5f308f938b9a69d9
z1kary <vicburet@orange.fr>
2020-11-28 01:43:55 +0000

    Creating new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,11 @@ class ArticlesController < ApplicationController
     def index 
         @articles = Article.all
     end
+    def new
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to article_path(@article)
+    end
 end
