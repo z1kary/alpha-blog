6b373748dfe74b2e29c33aa55d6052c3dfb52d2f
z1kary <vicburet@orange.fr>
2020-11-29 07:23:48 +0000

    Add users table and user model

---
+++ db/migrate/20201129065437_drop_column_article_user_id.rb
@@ -0,0 +1,5 @@
+class DropColumnArticleUserId < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :articles, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_112039) do
+ActiveRecord::Schema.define(version: 2020_11_29_071938) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "update_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
