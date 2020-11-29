cf0a9b531506b1a5ee10eb0d37e2529be2bf55d8
z1kary <vicburet@orange.fr>
2020-11-29 21:22:46 +0000

    Add admin user access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy ]
   
   def new
     @user = User.new
@@ -39,6 +39,13 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 5)
   end
   
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+  
   private
   
   def user_params
@@ -50,8 +57,8 @@ class UsersController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only on edit your own account"
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
       redirect_to @user
     end
   end
