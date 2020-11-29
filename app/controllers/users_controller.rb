929c738f05e9d4d3c4f8e141cac8f3b1d43248b7
z1kary <vicburet@orange.fr>
2020-11-29 16:31:37 +0000

    Add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'new'
     end
@@ -25,10 +25,10 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   
   private
