1b4fce93e536200fdd6acf4848ce0a2ea49187a3
z1kary <vicburet@orange.fr>
2020-11-29 11:23:56 +0000

    Add one-to-many association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 5, maximum: 60 }
     validates :description, presence: true, length: { minimum: 10, maximum: 255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_071938) do
+ActiveRecord::Schema.define(version: 2020_11_29_080202) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "update_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
