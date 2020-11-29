6d76fa19335fe66ab824709b0fdf5f9b0de414ed
z1kary <vicburet@orange.fr>
2020-11-29 12:46:53 +0000

    Add signup form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_080202) do
+ActiveRecord::Schema.define(version: 2020_11_29_120429) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_080202) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
